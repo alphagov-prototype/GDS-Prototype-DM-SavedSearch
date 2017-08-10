1ea16fdc00e768eff277d0bd1b1868579e53fe41
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-08-10 15:35:18 +0100

    search stages now working

--- app/routes.js
+++ app/routes.js
@@ -12,7 +12,24 @@ router.get('/g-cloud', function (req, res) {
 })
 
 router.get('/g-cloud/search', function (req, res) {
-  res.render('g-cloud/search')
+
+  var stage = req.query.stage;
+
+  switch( stage )
+  {
+    case "2":
+      res.render('g-cloud/search_stage2');
+      break;
+    case "3":
+      res.render('g-cloud/search_stage3');
+      break;
+    default:
+      res.render('g-cloud/search_stage1');
+      break;
+  }
+
+
+  
 })
 
 // add your routes here
